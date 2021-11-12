14e338fce28b6aebf2f909f21003d063cadbf1b0
Macbook <macbook@Macbooks-MacBook-Pro-MDL.local>
2021-11-11 21:18:46 -0400

    Entrega de Proyecto

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,60 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
+import React, { useState } from "react";
+import "bootstrap";
 //create your first component
 const Home = () => {
+	const [input, setInput] = useState("");
+
+	const [toDo, setToDo] = useState([]);
+
+	const handleKeyDown = e => {
+		if (e.key === "Enter") {
+			if (input.trim != "") {
+				setToDo([...toDo, input]);
+				setInput("");
+			}
+		}
+	};
+
+	const handleDeleteTask = id => {
+		const newArray = toDo.filter((toDo, index) => index != id);
+		setToDo(newArray);
+	};
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container div-father">
+			<p className="p1">todos</p>
+			<ul
+				className="list-group ul-list"
+				onSubmit={e => e.preventDefault()}>
+				<input
+					className="list-group input-important"
+					type="text"
+					placeholder="What needs to done?"
+					name="text"
+					value={input}
+					onChange={e => setInput(e.target.value)}
+					onKeyDown={handleKeyDown}
+				/>
+				{toDo.map((toDo, index) => {
+					return (
+						<li
+							className="list-group-item list-group-item-light li-one"
+							key={index}
+							onClick={() => {
+								handleDeleteTask(index);
+							}}>
+							{toDo}
+						</li>
+					);
+				})}
+				<ul className="list-group ul-list-two">
+					<li className="list-group-item list-group-item-light li-two">
+						{toDo.length <= 1
+							? `${toDo.length} item left`
+							: `${toDo.length} items left`}
+					</li>
+				</ul>
+			</ul>
 		</div>
 	);
 };
